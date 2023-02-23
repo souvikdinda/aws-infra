f057266b2f67058fafc793fecf34b8bf72b01a0c
Souvik Dinda <souvik.dinda.sd@gmail.com>
2023-02-22 23:48:24 -0500

    Added creation of ec2 instance

--- variables.tf
+++ variables.tf
@@ -13,3 +13,23 @@ data "aws_availability_zones" "azs" {
 variable "publicroutetablecidr" {
   default = "0.0.0.0/0"
 }
+
+data "aws_ami" "my-node-ami" {
+  most_recent      = true
+  owners           = ["377562592179"]
+
+  filter {
+    name   = "name"
+    values = ["NodeApp_*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
