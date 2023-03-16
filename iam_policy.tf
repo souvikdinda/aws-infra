cce511d9fae851069b271e670112fab3e5fa1e3c
Souvik Dinda <souvik.dinda.sd@gmail.com>
2023-03-16 02:39:38 -0400

    Added Route53 A record

--- iam_policy.tf
+++ iam_policy.tf
@@ -50,3 +50,8 @@ resource "aws_iam_policy_attachment" "policy-attachment" {
   roles      = ["${aws_iam_role.ec2-csye6225.name}"]
   policy_arn = aws_iam_policy.webapps3.arn
 }
+
+resource "aws_iam_instance_profile" "ec2-profile" {
+  name = "ec2-profile"
+  role = aws_iam_role.ec2-csye6225.name
+}
